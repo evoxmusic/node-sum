536af6548307a39cc3cfd1ac5aa379db35739bb9
Romaric Philogene <evoxmusic@gmail.com>
2020-02-20 23:43:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({"hello": "world"});
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
+
+module.exports = app;
